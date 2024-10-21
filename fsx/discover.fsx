3e252599dbcb46f2dae4ec30ed39bcdd29288342
queil <queil@users.noreply.github.com>
2024-10-21 11:35:27 +0000

    chore: remove unused functions

--- fsx/discover.fsx
+++ fsx/discover.fsx
@@ -19,12 +19,3 @@ module internal Discover =
     /// Find unique parent projects (determined by existence of a single file matching projectPattern) of the given dirs
     let uniqueParentProjectPaths (dirs: string seq) (projectPattern: string) : string seq =
         dirs |> Seq.choose (findParentProjectPath projectPattern) |> Seq.distinct
-
-    /// Find unique child projects (determined by existence of a single file matching projectPattern) in the given dir (recursively)
-    let uniqueChildProjectPaths (dir: string) (projectPattern: string) : string seq =
-        let rec findChildProj (acc: string list) (path: string) : string seq =
-            match Directory.EnumerateFiles(path, projectPattern) |> Seq.tryExactlyOne with
-            | Some s -> s :: acc
-            | None -> Directory.EnumerateDirectories(path) |> Seq.collect (findChildProj acc)
-
-        findChildProj [] dir
--- fsx/node.fsx
+++ fsx/node.fsx
@@ -8,5 +8,3 @@ module Node =
     let findParentProjects (dirs: string seq) =
         Discover.uniqueParentProjectPaths dirs "package.json" |> Seq.toList
 
-    let findProjects (dir: string) =
-        Discover.uniqueChildProjectPaths dir "package.json"
