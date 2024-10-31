6f060d9efd49ae2adf31e8b0381f21a6e2671170
queil <queil@users.noreply.github.com>
2024-10-31 12:10:08 +0000

    add: fs.WorkingDir

---
+++ fsx/fs.fsx
@@ -0,0 +1,14 @@
+namespace Arquidev.Dbt
+
+open System.IO
+
+[<RequireQualifiedAccess>]
+module Fs = 
+
+    type WorkingDir(workingDir: string) =
+        let originalDir = Directory.GetCurrentDirectory()
+        do Directory.SetCurrentDirectory workingDir
+
+        interface System.IDisposable with
+            member _.Dispose() : unit =
+                Directory.SetCurrentDirectory originalDir
