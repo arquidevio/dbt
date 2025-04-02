e5e80f3eabf9be288a14a93556232b46bb89f899
queil <4584075+queil@users.noreply.github.com>
2025-04-02 18:04:00 +0200

    Env wrapper (#12)

---
+++ fsx/env.fsx
@@ -0,0 +1,63 @@
+#r "paket: nuget TypeShape ~> 10"
+
+namespace Arquidev.Dbt
+
+open TypeShape.Core
+open System
+
+module Env =
+
+    let private parser<'V> : string -> 'V =
+        fun s ->
+            let wrap parse = unbox<'V> (parse s)
+
+            match shapeof<'V> with
+            | Shape.String -> wrap id
+            | Shape.Int32 -> wrap Int32.Parse
+            | Shape.Bool ->
+                wrap (function
+                    | "true" | "TRUE" | "1" | "YES" | "Y" -> true
+                    | "false" | "FALSE" | "0" | "NO" | "N" -> false
+                    | b -> failwithf $"Value {b} is not a valid boolean")
+            | shape -> failwithf $"Not supported: {shape}"
+
+    let getValue<'R> label =
+        let value = Environment.GetEnvironmentVariable label
+
+        match shapeof<'R> with
+        | Shape.FSharpOption shape ->
+            let optionElementParser =
+                shape.Element.Accept
+                    { new ITypeVisitor<string -> 'R> with
+                        member _.Visit<'a>() =
+                            function
+                            | s when String.IsNullOrEmpty s -> unbox<'R> None
+                            | s -> unbox<'R> (Some(parser<'a> s)) }
+
+            optionElementParser value
+        | _ ->
+            if String.IsNullOrEmpty value then
+                failwithf $"Environment variable is not set: {label}"
+
+            parser<'R> value
+
+    let get<'T> () =
+        match shapeof<'T> with
+        | Shape.FSharpRecord(:? ShapeFSharpRecord<'T> as shape) ->
+            let record = shape.CreateUninitialized()
+
+            let fieldSetters =
+                shape.Fields
+                |> Seq.fold
+                    (fun setters field ->
+                        field.Accept
+                            { new IMemberVisitor<'T, ('T -> 'T) list> with
+                                member _.Visit(shape: ShapeMember<'T, 'a>) =
+                                    (fun r ->
+                                        let fieldValue = getValue<'a> field.Label
+                                        shape.Set r fieldValue)
+                                    :: setters })
+                    []
+
+            fieldSetters |> Seq.fold (fun r setter -> setter r) record
+        | _ -> failwithf "Not supported"
---
+++ tests/test-env.fsx
@@ -0,0 +1,58 @@
+#r "paket: 
+    nuget Expecto ~> 10
+"
+#load "../fsx/env.fsx"
+
+open Expecto
+open Arquidev.Dbt
+
+let env =
+    Env.get<
+        {| OPTIONAL_EXISTS: string option
+           OPTIONAL_NOT_EXISTS: string option
+           REQUIRED_EXISTS: string
+           REQUIRED_INT: int
+           OPTIONAL_INT_EXISTS: int option |}
+     > ()
+
+"OPTIONAL_EXISTS - unexpected value"
+|> Expect.equal env.OPTIONAL_EXISTS (Some "XTYh1345")
+
+"OPTIONAL_NOT_EXISTS - unexpected value"
+|> Expect.equal env.OPTIONAL_NOT_EXISTS None
+
+"REQUIRED_EXISTS - unexpected value" |> Expect.equal env.REQUIRED_EXISTS "4t9v7"
+
+"REQUIRE_NOT_EXISTS should fail"
+|> Expect.throws (fun () -> Env.get<{| REQUIRE_NOT_EXISTS: string |}> () |> ignore)
+
+type MyEnv = { NORMAL_RECORD: int32 }
+
+let recordEnv = Env.get<MyEnv> ()
+
+"NORMAL_RECORD - unexpected value" |> Expect.equal recordEnv.NORMAL_RECORD 123
+
+let booleanEnv =
+    Env.get<
+        {| BOOL_TRUE: bool
+           BOOL_FALSE: bool
+           BOOL_TRUE_L: bool
+           BOOL_FALSE_L: bool
+           BOOL_1: bool
+           BOOL_0: bool
+           BOOL_YES: bool
+           BOOL_NO: bool
+           BOOL_Y: bool
+           BOOL_N: bool |}
+     > ()
+
+"BOOL_TRUE" |> Expect.equal booleanEnv.BOOL_TRUE true
+"BOOL_FALSE" |> Expect.equal booleanEnv.BOOL_FALSE false
+"BOOL_TRUE_L" |> Expect.equal booleanEnv.BOOL_TRUE_L true
+"BOOL_FALSE_L" |> Expect.equal booleanEnv.BOOL_FALSE_L false
+"BOOL_1" |> Expect.equal booleanEnv.BOOL_1 true
+"BOOL_0" |> Expect.equal booleanEnv.BOOL_0 false
+"BOOL_YES" |> Expect.equal booleanEnv.BOOL_YES true
+"BOOL_NO" |> Expect.equal booleanEnv.BOOL_NO false
+"BOOL_Y" |> Expect.equal booleanEnv.BOOL_Y true
+"BOOL_N" |> Expect.equal booleanEnv.BOOL_N false
