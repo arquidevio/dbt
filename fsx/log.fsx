010a3d3973a3f402c70e53ae2f71cac7a387829a
queil <4584075+queil@users.noreply.github.com>
2025-09-26 21:18:23 +0200

    breaking: use glob patterns + add some docs comments (#28)
    * breaking: use glob patterns + add some docs comments
    
    * fix: only group on debug
    
    * fix: put the exclude matcher to the right place

--- fsx/log.fsx
+++ fsx/log.fsx
@@ -20,6 +20,8 @@ module Log =
 
     let mutable private currentLevel = env.Value.DBT_LOG_LEVEL
 
+    let debugEnabled() = currentLevel >= LogLevel.debug
+
     let enableTrace (enable: bool) =
         if enable then
             currentLevel <- LogLevel.trace
